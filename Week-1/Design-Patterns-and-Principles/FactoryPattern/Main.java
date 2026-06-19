76376c7287cd5c11ba2197a1549b13e19ca112d9
DharshiniS22 <dharshinis220506@gmail.com>
2026-06-19 21:19:12 +0530

    Update Main.java

--- Week-1/Design-Patterns-and-Principles/FactoryPattern/Main.java
+++ Week-1/Design-Patterns-and-Principles/FactoryPattern/Main.java
@@ -1,1 +1,12 @@
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        DocumentFactory factory = new PdfFactory();
+
+        Document document = factory.createDocument();
+
+        document.open();
+    }
+}
