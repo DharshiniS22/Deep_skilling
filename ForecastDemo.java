4a6ae3bee1691d7c9406e6acfdfcc0271f5bb8c3
DharshiniS22 <dharshinis220506@gmail.com>
2026-07-03 18:27:52 +0530

    Add files via upload

---
+++ ForecastDemo.java
@@ -0,0 +1,20 @@
+public class ForecastDemo {
+
+    public static void main(String[] args) {
+
+        double presentValue = 10000;
+        double growthRate = 0.08; // 8%
+        int years = 5;
+
+        double futureValue = FinancialForecast.predictFutureValue(
+                presentValue,
+                growthRate,
+                years
+        );
+
+        System.out.println("Present Value : ₹" + presentValue);
+        System.out.println("Growth Rate   : " + (growthRate * 100) + "%");
+        System.out.println("Years         : " + years);
+        System.out.printf("Future Value  : ₹%.2f%n", futureValue);
+    }
+}
