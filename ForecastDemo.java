bde01a46ef0ba55577684f99114ffaa6e63c1f3f
DharshiniS22 <dharshinis220506@gmail.com>
2026-07-03 18:28:56 +0530

    Refactor growthRate declaration in ForecastDemo

--- ForecastDemo.java
+++ ForecastDemo.java
@@ -3,7 +3,7 @@ public class ForecastDemo {
     public static void main(String[] args) {
 
         double presentValue = 10000;
-        double growthRate = 0.08; // 8%
+        double growthRate = 0.08; 
         int years = 5;
 
         double futureValue = FinancialForecast.predictFutureValue(
